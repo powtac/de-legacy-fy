da105727dc20fabbf28cba4dfdc0edd8cb00612b
Simon Brüchner <s.bruechner@wawibox.de>
2019-10-15 13:27:42 +0200

    Make it capable of multiple entries and also render a generic test and provider into the output togehter with other multiple tests.

--- src/CharacterizationTestGenerator.php
+++ src/CharacterizationTestGenerator.php
@@ -19,15 +19,14 @@ use PHPUnit\Framework\TestCase;
 
 class %s extends TestCase
 {
-    /**
-     * @return array
-     */
-    public function provider()
-    {
-        return [
+
 %s
-        ];
+    protected function setUp(): void 
+    {
+        %s
     }
+
+%s
 
     /**
      * @param string $serializedValue
@@ -40,6 +39,31 @@ class %s extends TestCase
     }
 }
 ';
+	
+	private static $providerMethodTemplate = '
+    /**
+     * @return array
+     */
+    public function %sProvider()
+    {
+        return [
+%s
+        ];
+    }
+';
+
+	private static $testMethodTemplate = '
+    /**
+     * @return array
+     * @dataProvider %sProvider
+     */
+    public function test%s()
+    {
+        $args     = func_get_args();
+        $expected = array_shift($args);
+        $this->assertEquals($expected, %s);
+    }
+';
 
     /**
      * @param string $traceFile
@@ -47,37 +71,99 @@ class %s extends TestCase
      * @param string $testClass
      * @param string $testFile
      */
-    public function generate($traceFile, $unit, $testClass, $testFile)
+    public function generate($traceFile, $units, $testClass, $testFile)
     {
+    	$methodBuffer = '';
         $parser = new XdebugTraceParser;
-        $data   = $parser->parse($traceFile, $unit);
-        $buffer = '';
+		$units	= explode(',', $units);
+		$units	= array_filter($units, 'trim');
+		foreach ($units as $unit) {
+	        $data   = $parser->parse($traceFile, $unit);
+	        $buffer = '';
+	
+	
+	        for ($i = 0; $i < \count($data); $i++) {
+	            $last = $i == \count($data) - 1;
+	
+	            $buffer .= \sprintf(
+	                '            [%s]%s',
+	                \implode(
+	                    ', ',
+	                    \array_map(
+	                        function ($parameter) {
+	                            return '$this->decode(\'' . $parameter . '\')';
+	                        },
+	                        $data[$i]
+	                    )
+	                ),
+	                !$last ? ",\n" : ''
+	            );
+	        }
+	        
+	        $unitType = self::unittypeDetector($unit);
+			$classes = array();
+	        switch($unitType['type']) {
+	        	case 'function':
+	        		$caller = 'call_user_func_array(\''.$unitType['functionname'].'\', $args)';
+	        	break;
+	        	
+	        	case 'class':
+	        		$caller = 'call_user_func_array(array($this->'.$unitType['classname'].', \''.$unitType['method'].'\'), $args)';
+					
+					$classes[] = $unitType['classname'];
+	        	break;
+	        	
+	        	case 'static':
+	        		$caller = 'call_user_func_array(\''.$unitType['classname'].'::'.$unitType['method'].'\', $args)';
+	        	break;
+	        }
 
-        for ($i = 0; $i < \count($data); $i++) {
-            $last = $i == \count($data) - 1;
+			$params = '';
+			$setupCode = '';
+			foreach ($classes as $class) {
+				$params 	.= "    ".'protected $'.$class.';'."\n";
+				$setupCode 	.= '$this->'.$class.' = new '.$class.';'."\n";
+			}
 
-            $buffer .= \sprintf(
-                '            [%s]%s',
-                \implode(
-                    ', ',
-                    \array_map(
-                        function ($parameter) {
-                            return '$this->decode(\'' . $parameter . '\')';
-                        },
-                        $data[$i]
-                    )
-                ),
-                !$last ? ",\n" : ''
-            );
-        }
+	        $methodBuffer .= \sprintf(
+	            self::$testMethodTemplate,
+				$unitType['clean'],
+	            ucfirst($unitType['clean']),
+	            $caller
+	        );
+	        
+	        $methodBuffer .= \sprintf(
+	            self::$providerMethodTemplate,
+	            $unitType['clean'],
+	            $buffer
+	        );
+		}
 
         \file_put_contents(
             $testFile,
             \sprintf(
                 self::$classTemplate,
                 $testClass,
-                $buffer
+				$params,
+				$setupCode,
+                $methodBuffer
             )
         );
     }
+    
+    private static function unittypeDetector(string $unit) {
+    	if (strpos($unit, '->') !== false) {
+    		return array('type' => 'class', 'classname' => explode('->', $unit)[0], 'method' => explode('->', $unit)[1], 'clean' => explode('->', $unit)[0].ucfirst(explode('->', $unit)[1]));
+    	}
+    	
+    	if (strpos($unit, '::') !== false) {
+    		return array('type' => 'static', 'classname' => explode('::', $unit)[0], 'method' => explode('::', $unit)[1], 'clean' => explode('::', $unit)[0].ucfirst(explode('::', $unit)[1]));
+    	}
+    	
+    	return array('type' => 'function', 'functionname' => $unit, 'clean' => $unit);
+    }
 }
+
+
+
+
